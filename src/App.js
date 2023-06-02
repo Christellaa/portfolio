fdc2d7de0a300c7e4200a9fd66c3a6c06c62510c
Christella De Sousa <desousa.christella@proton.me>
2023-06-02 19:24:25 +0200

    Add studies route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Studies from "./pages/Studies/Studies";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/studies" element={<Studies />} />
       {/* <Route path="/projects" element={<Projects />} />
       <Route path="/skills" element={<Skills />} />
       <Route path="*" element={<Error />} /> */}
