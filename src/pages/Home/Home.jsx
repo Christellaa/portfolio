44793a8f816d13c61f2e831d04cce88f7a172aaf
Christella De Sousa <desousa.christella@proton.me>
2023-06-02 19:25:26 +0200

    Create homepage

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,50 @@
+import homeStyles from "./home.module.css";
+import LayoutStyles from "../../components/Layout/layout.module.css";
+
+export default function Home() {
+  return (
+    <div className={LayoutStyles.wrapper}>
+      <h1 className={homeStyles.textUppercase}>
+        Christella <br />
+        <span className={homeStyles.marginLeft}>De Sousa</span>
+      </h1>
+      <hr className={homeStyles.hr} />
+      <main>
+        <p>
+          Devenir développeuse est un choix de carrière passionnant, qui offre
+          de nombreuses opportunités de croissance professionnelle et
+          personnelle. En effet, ce métier permet de donner vie à nos idées dans
+          la création de sites web, en combiant notre créativité à nos
+          compétences dans le domaine.
+          <br />
+          La constante évolution des technologies garantit de toujours avoir des
+          opportunités d'améliorations et d'apprentissage, ainsi que de relever
+          de nouveaux défis en expérimentant des solutions innovantes.
+          <br />
+        </p>
+        <p>
+          Je suis ravie de me présenter en tant que développeuse web et de
+          partager ce que je peux offrir dans ce domaine. J'ai pu étudier, lors
+          de ma formation chez Openclassrooms, une base solide dans les
+          principes de développement web. J'ai pu me familiarisée avec plusieurs
+          langages de programmations, notamment Javascript et le framework
+          React. Mes compétences dans ces langages, je les ai aiguisés lors des
+          divers projets entrepris dans cette formation.
+        </p>
+        <p>
+          Bien que je sois développeuse junior, je suis très motivée et
+          désireuse d'apprendre. Je commence tout juste mon parcours dans le
+          monde du développement web, et je suis toujours à la recherche
+          d'opportunités pour élargir mes compétences et me tenir au courant des
+          dernières évolutions technologiques. <br />
+          Avec moi, j'apporte un fort déir d'apprendre, de grandir et d'avoir un
+          impact significatif dans ce domaine. Je suis consciente que mon
+          expérience peut être limitée, mais je suis impatiente de surmonter
+          tous les défis qui se présentent à moi. Je pense qu'êter un
+          développeur junior est un avantage unique car cela permet d'aborder
+          les projets avec une perspective nouvelle.
+        </p>
+      </main>
+    </div>
+  );
+}
