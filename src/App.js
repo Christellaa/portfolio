9af0c2eb22d7623177901fffee40cf29870f2873
Christella De Sousa <desousa.christella@proton.me>
2023-06-05 19:23:50 +0200

    Add path to skills page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Studies from "./pages/Studies/Studies";
 import Projects from "./pages/Projects/Projects";
+import Skills from "./pages/Skills/Skills";
 import "./App.css";
 
 function App() {
@@ -11,8 +12,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/studies" element={<Studies />} />
       <Route path="/projects" element={<Projects />} />
-      {/*<Route path="/skills" element={<Skills />} />
-      <Route path="*" element={<Error />} /> */}
+      <Route path="/skills" element={<Skills />} />
+      {/*<Route path="*" element={<Error />} /> */}
     </Routes>
   );
 }
