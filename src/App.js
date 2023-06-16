5343e707fc5fa5258db6887a85d4e503a2edc809
Christella De Sousa <desousa.christella@proton.me>
2023-06-16 15:19:51 +0200

    Add route to Error page and Project details page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Studies from "./pages/Studies/Studies";
 import Projects from "./pages/Projects/Projects";
+import ProjectDetails from "./pages/Projects/ProjectDetails";
 import Skills from "./pages/Skills/Skills";
+import Error from "./pages/Error";
 import "./App.css";
 
 function App() {
@@ -12,8 +14,9 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/studies" element={<Studies />} />
       <Route path="/projects" element={<Projects />} />
+      <Route path="/projects/:id" element={<ProjectDetails />} />
       <Route path="/skills" element={<Skills />} />
-      {/*<Route path="*" element={<Error />} /> */}
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 }
