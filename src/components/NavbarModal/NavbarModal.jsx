0d4c02c237cac5c7a23e55063cb6fd7ccb2bf89f
Christella De Sousa <desousa.christella@proton.me>
2023-06-16 15:10:26 +0200

    Add onclick to close modal when clicking the links

--- src/components/NavbarModal/NavbarModal.jsx
+++ src/components/NavbarModal/NavbarModal.jsx
@@ -1,36 +1,56 @@
 import { Link } from "react-router-dom";
 import cv from "../../assets/pdftest.pdf";
-import navbarModalStyles from "./navbarModal.module.css";
 
-export default function NavbarModal() {
+export default function NavbarModal({ setModalOpen }) {
   return (
-    <ul className={navbarModalStyles.modal}>
-      <li className={navbarModalStyles.noBullet}>
-        <Link className={navbarModalStyles.noDecoration} to="/">
+    <ul className="flexColumn flexCenter navbarModal">
+      <li className="noBullet">
+        <Link
+          className="noDecoration colorLinks"
+          to="/"
+          onClick={() => setModalOpen(false)}
+        >
           À Propos
         </Link>
       </li>
-      <hr className={navbarModalStyles.hr} />
-      <li className={navbarModalStyles.noBullet}>
-        <Link className={navbarModalStyles.noDecoration} to="/studies">
+      <hr className="navbarHr" />
+      <li className="noBullet">
+        <Link
+          className="noDecoration colorLinks"
+          to="/studies"
+          onClick={() => setModalOpen(false)}
+        >
           Parcours
         </Link>
       </li>
-      <hr className={navbarModalStyles.hr} />
-      <li className={navbarModalStyles.noBullet}>
-        <Link className={navbarModalStyles.noDecoration} to="/projects">
+      <hr className="navbarHr" />
+      <li className="noBullet">
+        <Link
+          className="noDecoration colorLinks"
+          to="/projects"
+          onClick={() => setModalOpen(false)}
+        >
           Projets
         </Link>
       </li>
-      <hr className={navbarModalStyles.hr} />
-      <li className={navbarModalStyles.noBullet}>
-        <Link className={navbarModalStyles.noDecoration} to="/skills">
+      <hr className="navbarHr" />
+      <li className="noBullet">
+        <Link
+          className="noDecoration colorLinks"
+          to="/skills"
+          onClick={() => setModalOpen(false)}
+        >
           Compétences
         </Link>
       </li>
-      <hr className={navbarModalStyles.hr} />
-      <li className={navbarModalStyles.noBullet}>
-        <Link to={cv} className={navbarModalStyles.noDecoration} target="blank">
+      <hr className="navbarHr" />
+      <li className="noBullet">
+        <Link
+          to={cv}
+          className="noDecoration colorLinks"
+          target="blank"
+          onClick={() => setModalOpen(false)}
+        >
           CV
         </Link>
       </li>
