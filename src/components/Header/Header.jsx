9f1021876a0751240b48f4269a6e1dadea8df2f3
Christella De Sousa <desousa.christella@proton.me>
2023-06-21 19:31:01 +0200

    Add a link to homepage on logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import Navbar from "../Navbar/Navbar";
 
 export default function Header() {
   return (
     <header className="flex flexCenter header">
-      <img src={logo} alt="Logo" />
+      <Link to="./">
+        <img src={logo} alt="Logo" />
+      </Link>
       <Navbar />
     </header>
   );
