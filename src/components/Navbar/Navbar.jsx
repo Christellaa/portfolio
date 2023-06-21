98e7a4a93f52ec5b765a02a17aecdded7004a444
Christella De Sousa <desousa.christella@proton.me>
2023-06-21 18:14:03 +0200

    Animate modal backdrop

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import NavbarModal from "../NavbarModal/NavbarModal";
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Navbar() {
   const [isModalOpen, setModalOpen] = useState(false);
@@ -22,15 +23,20 @@ export default function Navbar() {
           onClick={() => setModalOpen(false)}
         />
       )}
-      {isModalOpen && (
-        <>
-          <div
-            className="bgModalBackdrop"
-            onClick={(e) => setModalOpen(false)}
-          ></div>
-          <NavbarModal setModalOpen={setModalOpen} />
-        </>
-      )}
+      <AnimatePresence>
+        {isModalOpen && (
+          <>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0, transition: { delay: 0.2 } }}
+              className="bgModalBackdrop"
+              onClick={(e) => setModalOpen(false)}
+            ></motion.div>
+            <NavbarModal setModalOpen={setModalOpen} />
+          </>
+        )}
+      </AnimatePresence>
     </nav>
   );
 }
