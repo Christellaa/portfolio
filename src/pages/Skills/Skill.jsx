c6ce3ec9bd4c14ad27bed02142263574a357a2de
Christella De Sousa <desousa.christella@proton.me>
2023-06-21 18:04:43 +0200

    Create skill page

--- src/pages/Skills/Skill.jsx
+++ src/pages/Skills/Skill.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import SkillModal from "./SkillModal";
+import skillData from "../../data/skillData.json";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Skill({ title, skills }) {
   const [isModalOpen, setModalOpen] = useState(false);
@@ -9,18 +11,44 @@ export default function Skill({ title, skills }) {
   }
 
   return (
-    <div className="skillGrid">
-      <h2>{title}</h2>
-      {skills.map((content, id) => (
-        <img
-          key={id}
-          src={content.image}
-          className="small"
-          alt={content.altImage}
-          onClick={Toggle}
-        />
-      ))}
-      {isModalOpen && <SkillModal />}
+    <div>
+      <h2 className="textCenter">{title}</h2>
+      <div className="skillGrid">
+        {skills.map((content, id) => (
+          <div key={id}>
+            <img
+              src={content.image}
+              className="small skillIcon"
+              alt={content.altImage}
+              onClick={() => Toggle()}
+            />
+            {/* <h3>{content.name}</h3> */}
+          </div>
+        ))}
+      </div>
+      <AnimatePresence>
+        {isModalOpen && (
+          <>
+            {skillData.map((content, id) => (
+              <>
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0, transition: { delay: 0.2 } }}
+                  className="bgModalBackdrop"
+                  onClick={() => Toggle()}
+                ></motion.div>
+                <SkillModal
+                  key={id}
+                  name={content.skills.name}
+                  details={content.skills.details}
+                  Toggle={Toggle}
+                />
+              </>
+            ))}
+          </>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
