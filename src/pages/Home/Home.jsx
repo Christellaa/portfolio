1724b1c3765ae24e545d79b66ef5f64fbb2d6d66
Christella De Sousa <desousa.christella@proton.me>
2023-06-28 19:53:58 +0200

    Fix typos

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -23,7 +23,7 @@ export default function Home() {
           Je suis ravie de me présenter en tant que développeuse web et de
           partager ce que je peux offrir dans ce domaine. J'ai pu étudier, lors
           de ma formation chez Openclassrooms, une base solide dans les
-          principes de développement web. J'ai pu me familiarisée avec plusieurs
+          principes de développement web. J'ai pu me familiariser avec plusieurs
           langages de programmations, notamment Javascript et le framework
           React. Mes compétences dans ces langages, je les ai aiguisés lors des
           divers projets entrepris dans cette formation.
@@ -33,13 +33,14 @@ export default function Home() {
           désireuse d'apprendre. Je commence tout juste mon parcours dans le
           monde du développement web, et je suis toujours à la recherche
           d'opportunités pour élargir mes compétences et me tenir au courant des
-          dernières évolutions technologiques. <br />
+          dernières évolutions technologiques.
+          <br />
           Avec moi, j'apporte un fort désir d'apprendre, de grandir et d'avoir
           un impact significatif dans ce domaine. Je suis consciente que mon
           expérience peut être limitée, mais je suis impatiente de surmonter
           tous les défis qui se présentent à moi. Je pense qu'être un
           développeur junior est un avantage unique car cela permet d'aborder
-          les projets avec une perspective nouvelle.
+          les projets avec une nouvelle perspective.
         </p>
       </main>
     </div>
