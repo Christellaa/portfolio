190ddda4b42a310c970b5326e72611e766c0eb9e
Christella De Sousa <desousa.christella@proton.me>
2023-06-28 19:53:51 +0200

    Animate navbar modal links

--- src/components/NavbarModal/NavbarModal.jsx
+++ src/components/NavbarModal/NavbarModal.jsx
@@ -3,14 +3,63 @@ import cv from "../../assets/pdftest.pdf";
 import { motion } from "framer-motion";
 
 export default function NavbarModal({ isModalOpen, closeModal }) {
+  const variants = isModalOpen ? "opened" : "closed";
+  const menu = {
+    opened: {
+      scale: 1,
+      transition: { duration: 0.3, delay: 0.2 },
+    },
+    closed: {
+      scale: 0,
+      originY: 0,
+      originX: 1,
+      transition: { duration: 0.3, delay: 0.5 },
+    },
+  };
+  const link = {
+    opened: {
+      opacity: 1,
+      right: 0,
+      transition: { duration: 0.25, delay: 0.55 },
+    },
+    closed: {
+      opacity: 0,
+      right: 10,
+      transition: { duration: 0.3 },
+    },
+  };
+  const hr = {
+    opened: {
+      opacity: 1,
+      width: 60,
+      transition: { duration: 0.25, delay: 0.55 },
+    },
+    closed: {
+      opacity: 0,
+      width: 0,
+    },
+  };
+
   return (
     <motion.ul
       initial={{ scale: 0, originY: 0, originX: 1 }}
-      animate={{ scale: 1 }}
-      exit={{ scale: 0 }}
+      animate={variants}
+      variants={menu}
+      exit={{
+        scale: 0,
+        originY: 0,
+        originX: 1,
+        transition: { duration: 0.3, delay: 0.5 },
+      }}
       className="flexColumn flexCenter navbarModal"
     >
-      <li className="noBullet">
+      <motion.li
+        className="noBullet positionRelative"
+        initial={{ opacity: 0, right: 10 }}
+        animate={variants}
+        variants={link}
+        exit={{ opacity: 0, right: 10 }}
+      >
         <Link
           className="noDecoration colorLinks"
           to="/"
@@ -21,9 +70,21 @@ export default function NavbarModal({ isModalOpen, closeModal }) {
         >
           À Propos
         </Link>
-      </li>
-      <hr className="navbarHr" />
-      <li className="noBullet">
+      </motion.li>
+      <motion.hr
+        className="navbarHr"
+        initial={{ opacity: 0, width: 0 }}
+        animate={variants}
+        variants={hr}
+        exit={{ opacity: 0, width: 0 }}
+      />
+      <motion.li
+        className="noBullet positionRelative"
+        initial={{ opacity: 0, right: 10 }}
+        animate={variants}
+        variants={link}
+        exit={{ opacity: 0, right: 10 }}
+      >
         <Link
           className="noDecoration colorLinks"
           to="/studies"
@@ -34,9 +95,21 @@ export default function NavbarModal({ isModalOpen, closeModal }) {
         >
           Parcours
         </Link>
-      </li>
-      <hr className="navbarHr" />
-      <li className="noBullet">
+      </motion.li>
+      <motion.hr
+        className="navbarHr"
+        initial={{ opacity: 0, width: 0 }}
+        animate={variants}
+        variants={hr}
+        exit={{ opacity: 0, width: 0 }}
+      />
+      <motion.li
+        className="noBullet positionRelative"
+        initial={{ opacity: 0, right: 10 }}
+        animate={variants}
+        variants={link}
+        exit={{ opacity: 0, right: 10 }}
+      >
         <Link
           className="noDecoration colorLinks"
           to="/projects"
@@ -47,9 +120,21 @@ export default function NavbarModal({ isModalOpen, closeModal }) {
         >
           Projets
         </Link>
-      </li>
-      <hr className="navbarHr" />
-      <li className="noBullet">
+      </motion.li>
+      <motion.hr
+        className="navbarHr"
+        initial={{ opacity: 0, width: 0 }}
+        animate={variants}
+        variants={hr}
+        exit={{ opacity: 0, width: 0 }}
+      />
+      <motion.li
+        className="noBullet positionRelative"
+        initial={{ opacity: 0, right: 10 }}
+        animate={variants}
+        variants={link}
+        exit={{ opacity: 0, right: 10 }}
+      >
         <Link
           className="noDecoration colorLinks"
           to="/skills"
@@ -60,9 +145,21 @@ export default function NavbarModal({ isModalOpen, closeModal }) {
         >
           Compétences
         </Link>
-      </li>
-      <hr className="navbarHr" />
-      <li className="noBullet">
+      </motion.li>
+      <motion.hr
+        className="navbarHr"
+        initial={{ opacity: 0, width: 0 }}
+        animate={variants}
+        variants={hr}
+        exit={{ opacity: 0, width: 0 }}
+      />
+      <motion.li
+        className="noBullet positionRelative"
+        initial={{ opacity: 0, right: 10 }}
+        animate={variants}
+        variants={link}
+        exit={{ opacity: 0, right: 10 }}
+      >
         <Link
           to={cv}
           className="noDecoration colorLinks"
@@ -71,7 +168,7 @@ export default function NavbarModal({ isModalOpen, closeModal }) {
         >
           CV
         </Link>
-      </li>
+      </motion.li>
     </motion.ul>
   );
 }
