61d1a2b3a2f9e42437a34def520b0bf6575eda8b
Christella De Sousa <desousa.christella@proton.me>
2023-07-02 21:25:38 +0200

    Change names of const for animations and map links JSON file

--- src/components/NavbarModal/NavbarModal.jsx
+++ src/components/NavbarModal/NavbarModal.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
-import cv from "../../assets/pdftest.pdf";
 import { motion } from "framer-motion";
+import links from "../../data/links.json";
 
 export default function NavbarModal({ isModalOpen, closeModal }) {
   const variants = isModalOpen ? "opened" : "closed";
-  const menu = {
+  const showMenu = {
     opened: {
       scale: 1,
       transition: { duration: 0.3, delay: 0.2 },
@@ -16,7 +16,7 @@ export default function NavbarModal({ isModalOpen, closeModal }) {
       transition: { duration: 0.3, delay: 0.5 },
     },
   };
-  const link = {
+  const showLink = {
     opened: {
       opacity: 1,
       right: 0,
@@ -28,7 +28,7 @@ export default function NavbarModal({ isModalOpen, closeModal }) {
       transition: { duration: 0.3 },
     },
   };
-  const hr = {
+  const showHr = {
     opened: {
       opacity: 1,
       width: 60,
@@ -44,7 +44,7 @@ export default function NavbarModal({ isModalOpen, closeModal }) {
     <motion.ul
       initial={{ scale: 0, originY: 0, originX: 1 }}
       animate={variants}
-      variants={menu}
+      variants={showMenu}
       exit={{
         scale: 0,
         originY: 0,
@@ -53,122 +53,56 @@ export default function NavbarModal({ isModalOpen, closeModal }) {
       }}
       className="flexColumn flexCenter navbarModal"
     >
-      <motion.li
-        className="noBullet positionRelative"
-        initial={{ opacity: 0, right: 10 }}
-        animate={variants}
-        variants={link}
-        exit={{ opacity: 0, right: 10 }}
-      >
-        <Link
-          className="noDecoration colorLinks"
-          to="/"
-          onClick={() => {
-            closeModal();
-            window.scrollTo(0, 0);
-          }}
-        >
-          À Propos
-        </Link>
-      </motion.li>
-      <motion.hr
-        className="navbarHr"
-        initial={{ opacity: 0, width: 0 }}
-        animate={variants}
-        variants={hr}
-        exit={{ opacity: 0, width: 0 }}
-      />
-      <motion.li
-        className="noBullet positionRelative"
-        initial={{ opacity: 0, right: 10 }}
-        animate={variants}
-        variants={link}
-        exit={{ opacity: 0, right: 10 }}
-      >
-        <Link
-          className="noDecoration colorLinks"
-          to="/studies"
-          onClick={() => {
-            closeModal();
-            window.scrollTo(0, 0);
-          }}
-        >
-          Parcours
-        </Link>
-      </motion.li>
-      <motion.hr
-        className="navbarHr"
-        initial={{ opacity: 0, width: 0 }}
-        animate={variants}
-        variants={hr}
-        exit={{ opacity: 0, width: 0 }}
-      />
-      <motion.li
-        className="noBullet positionRelative"
-        initial={{ opacity: 0, right: 10 }}
-        animate={variants}
-        variants={link}
-        exit={{ opacity: 0, right: 10 }}
-      >
-        <Link
-          className="noDecoration colorLinks"
-          to="/projects"
-          onClick={() => {
-            closeModal();
-            window.scrollTo(0, 0);
-          }}
-        >
-          Projets
-        </Link>
-      </motion.li>
-      <motion.hr
-        className="navbarHr"
-        initial={{ opacity: 0, width: 0 }}
-        animate={variants}
-        variants={hr}
-        exit={{ opacity: 0, width: 0 }}
-      />
-      <motion.li
-        className="noBullet positionRelative"
-        initial={{ opacity: 0, right: 10 }}
-        animate={variants}
-        variants={link}
-        exit={{ opacity: 0, right: 10 }}
-      >
-        <Link
-          className="noDecoration colorLinks"
-          to="/skills"
-          onClick={() => {
-            closeModal();
-            window.scrollTo(0, 0);
-          }}
-        >
-          Compétences
-        </Link>
-      </motion.li>
-      <motion.hr
-        className="navbarHr"
-        initial={{ opacity: 0, width: 0 }}
-        animate={variants}
-        variants={hr}
-        exit={{ opacity: 0, width: 0 }}
-      />
-      <motion.li
-        className="noBullet positionRelative"
-        initial={{ opacity: 0, right: 10 }}
-        animate={variants}
-        variants={link}
-        exit={{ opacity: 0, right: 10 }}
-      >
-        <Link
-          to={cv}
-          className="noDecoration colorLinks"
-          target="blank"
-          onClick={closeModal}
-        >
-          CV
-        </Link>
-      </motion.li>
+      {links.map((link, id) => (
+        <>
+          {link.openInNewTab === true ? (
+            <motion.li
+              key={id}
+              className="noBullet positionRelative"
+              initial={{ opacity: 0, right: 10 }}
+              animate={variants}
+              variants={showLink}
+              exit={{ opacity: 0, right: 10 }}
+            >
+              <Link
+                className="noDecoration colorLinks"
+                to={link.url}
+                target="_blank"
+              >
+                {link.name}
+              </Link>
+            </motion.li>
+          ) : (
+            <div key={id}>
+              <motion.li
+                className="noBullet positionRelative"
+                initial={{ opacity: 0, right: 10 }}
+                animate={variants}
+                variants={showLink}
+                exit={{ opacity: 0, right: 10 }}
+              >
+                <Link
+                  className="noDecoration colorLinks"
+                  to={link.url}
+                  onClick={() => {
+                    closeModal();
+                    window.scrollTo(0, 0);
+                  }}
+                >
+                  {link.name}
+                </Link>
+              </motion.li>
+              <motion.hr
+                className="navbarHr"
+                initial={{ opacity: 0, width: 0 }}
+                animate={variants}
+                variants={showHr}
+                exit={{ opacity: 0, width: 0 }}
+              />
+            </div>
+          )}
+        </>
+      ))}
     </motion.ul>
   );
 }
