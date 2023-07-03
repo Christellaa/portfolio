869aab21d94f4f031b7fd940b253f9ae25530518
Christella De Sousa <desousa.christella@proton.me>
2023-07-03 10:28:59 +0200

    Create error page

---
+++ src/pages/Error.jsx
@@ -0,0 +1,8 @@
+export default function Error() {
+  return (
+    <div className="wrapper flexColumn error textCenter">
+      <h1>404</h1>
+      <p>La page que vous cherchez n'existe pas.</p>
+    </div>
+  );
+}
