02e32651d3d79389a218890bb71d58199033adbf
Christella De Sousa <desousa.christella@proton.me>
2023-09-08 15:48:32 +0200

    Modify text

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,48 +1,20 @@
 export default function Home() {
   return (
-    <div className="wrapper">
-      <h1 className="textUppercase">
-        Christella <br />
-        <span className="marginLeft">De Sousa</span>
-      </h1>
-      <hr className="hr" />
-      <main>
-        <p>
-          Devenir développeuse est un choix de carrière passionnant, qui offre
-          de nombreuses opportunités de croissance professionnelle et
-          personnelle. En effet, ce métier permet de donner vie à nos idées dans
-          la création de sites web, en combinant notre créativité à nos
-          compétences dans le domaine.
-          <br />
-          La constante évolution des technologies garantit de toujours avoir des
-          opportunités d'améliorations et d'apprentissage, ainsi que de relever
-          de nouveaux défis en expérimentant des solutions innovantes.
-          <br />
-        </p>
-        <p>
-          Je suis ravie de me présenter en tant que développeuse web et de
-          partager ce que je peux offrir dans ce domaine. J'ai pu étudier, lors
-          de ma formation chez Openclassrooms, une base solide dans les
-          principes de développement web. J'ai pu me familiariser avec plusieurs
-          langages de programmations, notamment Javascript et le framework
-          React. Mes compétences dans ces langages, je les ai aiguisés lors des
-          divers projets entrepris dans cette formation.
-        </p>
-        <p>
-          Bien que je sois développeuse junior, je suis très motivée et
-          désireuse d'apprendre. Je commence tout juste mon parcours dans le
-          monde du développement web, et je suis toujours à la recherche
-          d'opportunités pour élargir mes compétences et me tenir au courant des
-          dernières évolutions technologiques.
-          <br />
-          Avec moi, j'apporte un fort désir d'apprendre, de grandir et d'avoir
-          un impact significatif dans ce domaine. Je suis consciente que mon
-          expérience peut être limitée, mais je suis impatiente de surmonter
-          tous les défis qui se présentent à moi. Je pense qu'être un
-          développeur junior est un avantage unique car cela permet d'aborder
-          les projets avec une nouvelle perspective.
-        </p>
-      </main>
+          <p>
+            Ayant récemment été diplômée en tant que intégratrice web, je suis
+            enthousiaste à l'idée de poursuivre ma carrière dans le
+            développement web. mon parcours chez OpenClassrooms m'a offert une
+            solide formation, dans laquelle mes compétences se sont aiguisées
+            grâce aux divers projets proposés.
+          </p>
+          <p>
+            En ce début de carrière, je me spécialise dans la création de sites
+            web côté front-end. Je crée des sites intuitifs, réactifs et
+            élégants, donnant vie à vos idées en ligne tout en alliant
+            fonctionnalité et esthétique pour renforcer votre présence sur le
+            web.
+          </p>
+        </main>
     </div>
   );
 }
