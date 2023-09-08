364e656507122c884bc9ebbc1d99510ede068c36
Christella De Sousa <desousa.christella@proton.me>
2023-09-08 15:51:13 +0200

    Delete link to mail

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,4 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { Link } from "react-router-dom";
@@ -8,15 +7,6 @@ export default function Footer() {
   return (
     <footer>
       <ul className="flex flexCenter footer">
-        <li className="noBullet">
-          <Link
-            className="noDecoration colorLinks hoveredLink"
-            to="mailto:desousa.christella@proton.me"
-            aria-label="Envoyer un email"
-          >
-            <FontAwesomeIcon icon={faEnvelope} className="icon" />
-          </Link>
-        </li>
         <li className="noBullet">
           <Link
             className="noDecoration colorLinks hoveredLink"
