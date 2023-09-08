86c2a18ff2352605266f84b185eae191087245a1
Christella De Sousa <desousa.christella@proton.me>
2023-09-08 15:51:44 +0200

    Add link to social networks

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import fiverrIcon from "../../assets/fiverrIcon.svg";
+import upworkIcon from "../../assets/upworkIcon.svg";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
@@ -26,6 +28,38 @@ export default function Footer() {
             aria-label="Voir le compte GitHub"
           >
             <FontAwesomeIcon icon={faGithub} className="icon" />
+          </Link>
+        </li>
+        <li className="noBullet">
+          <Link
+            className="noDecoration colorLinks hoveredLink"
+            to="https://fr.fiverr.com/christellaa_"
+            target="blank"
+            aria-label="Voir le compte Fiverr"
+          >
+            <img
+              src={fiverrIcon}
+              className="icon"
+              width="28"
+              height="28"
+              alt="Icône de Fiverr"
+            />
+          </Link>
+        </li>
+        <li className="noBullet">
+          <Link
+            className="noDecoration colorLinks hoveredLink"
+            to="https://www.upwork.com/freelancers/~01a83b2cc0a789ca2c"
+            target="blank"
+            aria-label="Voir le compte Upwork"
+          >
+            <img
+              src={upworkIcon}
+              className="icon"
+              width="25"
+              height="25"
+              alt="Icône de Upwork"
+            />
           </Link>
         </li>
       </ul>
