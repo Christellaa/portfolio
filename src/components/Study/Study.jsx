14f751fed09720e8e87306dc527476b3af02a632
Christella De Sousa <desousa.christella@proton.me>
2023-09-15 17:26:44 +0200

    Add hat with bg around it

--- src/components/Study/Study.jsx
+++ src/components/Study/Study.jsx
@@ -1,14 +1,22 @@
 import StudyDescription from "./StudyDescription";
+import hat from "../../assets/hat.svg";
 
 export default function Study({ title, place, year, description }) {
   return (
-    <article className="items studyGridItem">
-      <h2 className="studyTitle">{title}</h2>
-      <h3 className="place">{place}</h3>
-      <p className="date">{year}</p>
-      <ul className="noBullet">
-        <StudyDescription description={description} />
-      </ul>
+    <article className="flex items studyGridItem">
+      <div className="studyLeft">
+        <h2 className="studyTitle">{title}</h2>
+        <h3 className="place">{place}</h3>
+        <p className="date">{year}</p>
+        <ul className="noBullet">
+          <StudyDescription description={description} />
+        </ul>
+      </div>
+      <div>
+        <span className="flex flexCenter hatBg">
+          <img src={hat} className="hat" alt="" />
+        </span>
+      </div>
     </article>
   );
 }
