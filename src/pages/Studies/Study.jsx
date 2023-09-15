9587881e86333165f3694045640310142849e863
Christella De Sousa <desousa.christella@proton.me>
2023-09-15 16:23:24 +0200

    Modify class name

--- src/pages/Studies/Study.jsx
+++ src/pages/Studies/Study.jsx
@@ -3,7 +3,7 @@ import StudyDescription from "./StudyDescription";
 export default function Study({ title, place, year, description }) {
   return (
     <article className="items studyGridItem">
-      <h2 className="title">{title}</h2>
+      <h2 className="studyTitle">{title}</h2>
       <h3 className="place">{place}</h3>
       <p className="date">{year}</p>
       <ul className="noBullet">
