7d2e0d91997d0c283c3adb1b8d8ad99fed8f6cb4
Christella De Sousa <desousa.christella@proton.me>
2023-09-15 17:50:01 +0200

    Add link to other projects on GitHub

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,10 +1,21 @@
+import { Link } from "react-router-dom";
 import ProjectCard from "../../components/Project/ProjectCard";
 
 export default function Projects() {
   return (
     <div className="wrapper">
       <h1>Mes Projets</h1>
-      <p>Voici une partie des projets que j'ai réalisés.</p>
+      <p>
+        Voici une partie des projets que j'ai réalisés.
+        <br /> Pour voir les autres projets, cliquez&nbsp;
+        <Link
+          to="https://github.com/Christellaa?tab=repositories"
+          target="_blank"
+        >
+          ici
+        </Link>
+        .
+      </p>
       <main className="grid gridGap gridLargeScreen">
         <ProjectCard />
       </main>
