29005d66b56cc145446b8482361a1592bc8cf666
Christella De Sousa <desousa.christella@proton.me>
2023-09-19 17:11:30 +0200

    Show content when hovering the card

--- src/components/Project/ProjectCard.jsx
+++ src/components/Project/ProjectCard.jsx
@@ -3,80 +3,56 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import projectData from "../../data/projectData.json";
-import websiteImg from "../../assets/website.svg";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "framer-motion";
 
 export default function ProjectCard() {
-  const projectCard = { opacity: 0.8, scale: 1.05 };
+  const projectCard = { translateY: 10 };
 
   return (
     <>
-      {projectData.map(
-        ({
-          id,
-          title,
-          imageDesktop,
-          imageMobile,
-          altImage,
-          website,
-          linkCode,
-        }) => (
-          <Link
-            onClick={() => window.scrollTo(0, 0)}
-            to={`/projects/${id}`}
-            className="noDecoration colorBtn"
-            key={id}
+      {projectData.map(({ id, title, image, altImage, website, linkCode }) => (
+        <Link
+          onClick={() => window.scrollTo(0, 0)}
+          to={`/projects/${id}`}
+          className="noDecoration colorBtn"
+          key={id}
+        >
+          <motion.article
+            className="flexColumn items cardContainer"
+            whileHover={projectCard}
           >
-            <motion.article
-              className="flexColumn items"
-              whileHover={projectCard}
-            >
-              <div className="imgContainer">
-                <img
-                  src={imageDesktop}
-                  alt={altImage}
-                  height="165"
-                  width="270"
-                  loading="lazy"
-                />
-                <img
-                  src={imageMobile}
-                  alt={altImage}
-                  height="165"
-                  width="122"
-                  loading="lazy"
-                />
-              </div>
-              <div className="flex flexCenter projectSpace">
-                <h2>{title}</h2>
-                <span className="flex space">
-                  {website !== null && (
-                    <img
-                      src={websiteImg}
-                      alt="website"
-                      width="23"
-                      height="23"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        window.open(website, "_blank");
-                      }}
-                    />
-                  )}
+            <img src={image} alt={altImage} />
+            <div className="cardInfo">
+              <h2 className="cardTitle">{title}</h2>
+              <span>
+                {website !== null && (
                   <FontAwesomeIcon
-                    icon={faGithub}
-                    className="icon"
+                    icon={faGlobe}
+                    className="icon iconSpace"
                     onClick={(e) => {
                       e.preventDefault();
-                      window.open(linkCode, "_blank");
+                      window.open(website, "_blank");
                     }}
                   />
-                  <FontAwesomeIcon icon={faArrowRight} className="icon" />
-                </span>
-              </div>
-            </motion.article>
-          </Link>
-        )
-      )}
+                )}
+                <FontAwesomeIcon
+                  icon={faGithub}
+                  className="icon iconSpace"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    window.open(linkCode, "_blank");
+                  }}
+                />
+                <FontAwesomeIcon
+                  icon={faArrowRight}
+                  className="icon iconSpace"
+                />
+              </span>
+            </div>
+          </motion.article>
+        </Link>
+      ))}
     </>
   );
 }
